06570cb85df9e64bd4124e7264bf762f5a99dc15
Luan Oliveira Santos <luanoliveirasantos@hotmail.com>
2020-05-18 13:06:57 -0300

    Estudo de programação orientada a objetos(POO)

---
+++ poo/Aviao.java
@@ -0,0 +1,15 @@
+package poo;
+
+public class Aviao extends Carro {
+	
+	double envergadura;
+	
+	void aterrizar() {
+		System.out.println("---------____________");
+	}
+	
+	void acelerar() {
+		System.out.println("__________-----------");
+	}
+	
+}
---
+++ poo/Camaro.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Camaro {
+
+	public static void main(String[] args) {
+		Carro camaro = new Carro();
+		camaro.ano = 1968;
+		camaro.cor = "Amarelo";
+		System.out.println("Carro: Camaro");
+		System.out.println("Ano: " + camaro.ano);
+		System.out.println("Cor: " + camaro.cor);
+		camaro.ligar();
+		camaro.acelerar();
+		camaro.desligar();
+
+	}
+
+}
